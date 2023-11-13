b6cee5704572be7217d536f49d6dd147335bed67
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-11-13 13:07:19 -0500

    z f
    zf d

---
+++ bon-appe-vite/WAF/keycaps.js
@@ -0,0 +1,137 @@
+const keycaps = [
+
+    {
+
+        name: "KiiBOOM Transparent PC Keycaps",
+        price: 19.99,
+        inStock: false,
+        multicolors: [true, "clear", "pink", "purple"],
+        profile: "ASA",
+        notes: "made of polycarbonate and fits on keyswitches with the cross stem only"
+
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+
+    {
+
+        name: "keycap_name",
+        price: x,    
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+    {
+
+        name: "keycap_name",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "",
+        notes: ""
+
+    },
+
+];
