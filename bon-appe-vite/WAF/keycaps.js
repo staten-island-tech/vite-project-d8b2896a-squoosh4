506bbedf11cfa56f187ed8be42e4f3915f949511
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-11-14 13:15:12 -0500

    fbdx
    txgd

--- bon-appe-vite/WAF/keycaps.js
+++ bon-appe-vite/WAF/keycaps.js
@@ -7,9 +7,58 @@ const keycaps = [
         inStock: false,
         multicolors: [true, "clear", "pink", "purple"],
         profile: "ASA",
-        notes: "made of polycarbonate and fits on keyswitches with the cross stem only"
+        notes: "made of polycarbonate and fits on keyswitches with the cross stem only",
+        isoOrANSI: "BOTH",
+        keycapType: "polycarbonate",
 
+    },
 
+    {
+
+        name: "Tai-Hao Sunshine 116 Key",
+        price: 37.05,
+        inStock: true,
+        multicolors: false,
+        profile: "OEM",
+        notes: "double shot pbt keycaps with cool coloring",
+        isoOrANSI: "ansi",
+        keycapType: "",
+    },
+
+    {
+
+        name: "MK x Ducky Frozen Llama PBT",
+        price: 39.20,
+        inStock: false,
+        multicolors: false,
+        profile: "OEM",
+        notes: "pruple blue and teal",
+        isoOrANSI: "ISO",
+        keycapType: "PBT",
+    },
+
+    {
+
+        name: "Ducky Black Pudding 108 Key",
+        price: 39.00,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "OEM",
+        notes: "good pudding keycap set",
+        isoOrANSI: "",
+        keycapType: "",
+    },
+
+    {
+
+        name: "XVX Pudding Keycaps",
+        price: x,
+        inStock: flue,
+        multicolors: [true, "black", "white"],
+        profile: "",
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -19,8 +68,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -30,52 +80,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
-    },
-
-    {
-
-        name: "keycap_name",
-        price: x,
-        inStock: flue,
-        multicolors: trulse,
-        profile: "",
-        notes: ""
-
-    },
-
-    {
-
-        name: "keycap_name",
-        price: x,
-        inStock: flue,
-        multicolors: trulse,
-        profile: "",
-        notes: ""
-
-    },
-
-    {
-
-        name: "keycap_name",
-        price: x,
-        inStock: flue,
-        multicolors: trulse,
-        profile: "",
-        notes: ""
-
-    },
-
-    {
-
-        name: "keycap_name",
-        price: x,
-        inStock: flue,
-        multicolors: trulse,
-        profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
 
@@ -86,8 +93,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -97,8 +105,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -108,8 +117,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -119,8 +129,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
     {
@@ -130,8 +141,9 @@ const keycaps = [
         inStock: flue,
         multicolors: trulse,
         profile: "",
-        notes: ""
-
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
     },
 
 ];
