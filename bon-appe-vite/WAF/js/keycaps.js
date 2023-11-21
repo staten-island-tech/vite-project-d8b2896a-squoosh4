a1b2411816c9d989c1fcb5b97ac6d871c619ffb8
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-11-21 11:59:07 -0500

    iuser;ou hdg ;ouh
     lihsre liy ;iublq

---
+++ bon-appe-vite/WAF/js/keycaps.js
@@ -0,0 +1,86 @@
+const keycaps = [
+
+    {
+
+        name: "KiiBOOM Transparent PC Keycaps",
+        price: 19.99,
+        inStock: false,
+        multicolors: [true, "clear", "pink", "purple"],
+        profile: "ASA",
+        notes: "made of polycarbonate and fits on keyswitches with the cross stem only",
+        isoOrANSI: "BOTH",
+        keycapType: "polycarbonate",
+        onSale: false,
+
+    },
+
+    {
+
+        name: "Tai-Hao Sunshine 116 Key",
+        price: 37.05,
+        inStock: true,
+        multicolors: false,
+        profile: "OEM",
+        notes: "double shot pbt keycaps with cool coloring",
+        isoOrANSI: "ANSI",
+        keycapType: "PBT",
+        onSale: false,
+
+    },
+
+    {
+
+        name: "MK x Ducky Frozen Llama PBT",
+        price: 39.20,
+        inStock: false,
+        multicolors: false,
+        profile: "OEM",
+        notes: "pruple blue and teal",
+        isoOrANSI: "BOTH",
+        keycapType: "PBT",
+        onSale: false,
+
+    },
+
+    {
+
+        name: "Ducky Black Pudding 108 Key",
+        price: 39.00,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "OEM",
+        notes: "good pudding keycap set",
+        isoOrANSI: "ANSI",
+        keycapType: "",
+        onSale: false,
+
+    },
+
+    {
+
+        name: "XVX Pudding Keycaps",
+        price: 14.99,
+        inStock: flue,
+        multicolors: [true, "black", "white"],
+        profile: "OEM",
+        notes: "budget pudding keycap set with a good typeface for the legending    ",
+        isoOrANSI: "ANSI",
+        keycapType: "",
+        onSale: true,
+
+    },
+
+    {
+
+        name: "HyperX Pudding Keycaps",
+        price: x,
+        inStock: flue,
+        multicolors: trulse,
+        profile: "OEM",
+        notes: "",
+        isoOrANSI: "",
+        keycapType: "",
+        onSale: boolean,
+    },
+
+];
