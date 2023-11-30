310ce30ae9c071250d4a8beed4247a9abf2b7146
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-11-29 20:55:18 -0500

    sgr
    sgr

--- bon-appe-vite/main.js
+++ bon-appe-vite/main.js
@@ -12,49 +12,58 @@ import { DOMSelector } from './js/domselector.js'
 
   // }
 
+function onlyShowThis(onlyShowThis){
 
-DOMSelector.buttons.purpleMintThemeButton.addEventListener("click", function(event){
+	document.querySelectorAll("#" + onlyShowThis);
+	DOMSelector.keycapCard.forEach((card) => keycapCard.innerHTML.style("display: hidden"))
+	DOMSelector.keycapCard.forEach((card) => card.filter((wantedCard) => wantedCard.classList.contains(onlyShowThis)))
 
-  DOMSelector.body.classList.add("purple-mint-theme")
-  DOMSelector.body.classList.remove("dark-theme")
+}
+  
+DOMSelector.buttons.choccoMintThemeButton.addEventListener("click", function(event){
 
+    DOMSelector.body.classList.add("chocco-mint-theme")
+    DOMSelector.body.classList.remove("dark-theme")
+    DOMSelector.body.classList.remove("purple-mint-theme")
+    
 });
 
 DOMSelector.buttons.darkThemeButton.addEventListener("click", function(event){
 
-  DOMSelector.body.classList.add("dark-theme")
-  DOMSelector.body.classList.remove("purple-mint-theme")
+    DOMSelector.body.classList.remove("chocco-mint-theme")
+    DOMSelector.body.classList.add("dark-theme")
+    DOMSelector.body.classList.remove("purple-mint-theme")
   
 });
 
+DOMSelector.buttons.purpleMintThemeButton.addEventListener("click", function(event){
+
+	DOMSelector.body.classList.remove("chocco-mint-theme")
+	DOMSelector.body.classList.remove("dark-theme")
+	DOMSelector.body.classList.add("purple-mint-theme")
+
+});
 
 
 keycaps.forEach((keycap) => DOMSelector.body.insertAdjacentHTML(
-  "beforeend",
-  `
-  <div class="keycap-card" id="${keycap.profile} ${keycap.name}">
-    <p>$${keycap.price}</p>
-    <p>on sale:
-    ${//if (keycap.onSale === true) {
-    //    this.innerHTML = `
-    //    on sale!
-    //    `
-    // }
-    keycap.onSale}
-    </p>
-    <p>${keycap.profile} profile</p>
-    <p>in stock: ${keycap.inStock}</p>
-    <p>multiple colorways: ${keycap.multicolors[0]}</p>
-    <div class="data">
-  </div>
-  `
+    "beforeend",
+    `
+      <div class="keycap-card" id="${keycap.profile} ${keycap.name}">
+        <p>$${keycap.price}</p>
+        <p>on sale:
+        ${//if (keycap.onSale === true) {
+        //    this.innerHTML = `
+        //    on sale!
+        //    `
+        // }
+        keycap.onSale}
+        </p>
+        <p>${keycap.profile} profile</p>
+        <p>in stock: ${keycap.inStock}</p>
+        <p>multiple colorways: ${keycap.multicolors[0]}</p>
+      </div>
+    `
 ));
-
-function onlyShowThis(onlyShowThis){
-  document.querySelectorAll("#" + onlyShowThis);
-  DOMSelector.keycapCard.forEach((card) => keycapCard.innerHTML.style("display: hidden"))
-  DOMSelector.keycapCard.forEach((card) => card.filter((wantedCard) => wantedCard.classList.contains(onlyShowThis)))
-}
 
 DOMSelector.buttons.forEach((btn) => btn.addEventListener("click", function(event){
 
