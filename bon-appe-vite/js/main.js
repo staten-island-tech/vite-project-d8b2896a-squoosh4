270f0737905c095d0bd0b96329301ff910c69c84
squoosh4 <110200988+squoosh4@users.noreply.github.com>
2023-12-06 13:13:28 -0500

    ersg
    gres

--- bon-appe-vite/js/main.js
+++ bon-appe-vite/js/main.js
@@ -2,31 +2,31 @@ import '../css/style.css'
 import { keycaps } from './keycaps.js'
 import { DOMSelector } from './domselector.js'
 
-  // cardObj = {
+// cardObj = {
 
-  //     name: keycaps.name,
-  //     price: keycaps.price,
-  //     onSale: keycaps.onSale,
-  //     profile: keycaps.profile,
-  //     inStock: keycaps.inStock,
+//     name: keycaps.name,
+//     price: keycaps.price,
+//     onSale: keycaps.onSale,
+//     profile: keycaps.profile,
+//     inStock: keycaps.inStock,
 
-  // }
-function cardCreate(arr){
+// }
+function cardCreate(arr) {
 
-  DOMSelector.cardHolder.innerHTML=""
-  arr.forEach((item) => DOMSelector.cardHolder.insertAdjacentHTML(
-    "beforeend",
-    `
+	DOMSelector.cardHolder.innerHTML = ""
+	arr.forEach((item) => DOMSelector.cardHolder.insertAdjacentHTML(
+		"beforeend",
+		`
       <div class="keycap-card" id="${item.profile} ${item.name} ${item.onSale}">
         <p>${item.name}</p>
         <p>$${item.price}</p>
         <p>on sale:
         ${//if (item.onSale === true) {
-        //    this.innerHTML = `
-        //    on sale!
-        //    `
-        // }
-        item.onSale}
+		//    this.innerHTML = `
+		//    on sale!
+		//    `
+		// }
+		item.onSale}
         </p>
         <p>iso/ansi: ${item.isoOrANSI}</p>
         <p>${item.profile} profile</p>
@@ -34,92 +34,95 @@ function cardCreate(arr){
         <p>multiple colorways: ${item.multicolors[0]}</p>
       </div>
     `
-));
+	));
 };
 
 cardCreate(keycaps);
 
-function onlyShowThis(onlyShowThis){
+function onlyShowThis(onlyShowThis) {
 
 	//document.querySelectorAll("#" + onlyShowThis);
 	//DOMSelector.keycapCard.forEach((card) => { if (card.id !== onlyShowThis)  
-  //  card.innerHTML.style.display("none")});
-  DOMSelector.cardHolder.innerHTML="";
-  const wanted_thing = keycaps.filter((card) => keycaps.includes(onlyShowThis));
-  cardCreate(wanted_thing);
+	//  card.innerHTML.style.display("none")});
+	DOMSelector.cardHolder.innerHTML = "";
+	const wanted_thing = keycaps.filter((card) => keycaps.includes(onlyShowThis));
+	cardCreate(wanted_thing);
 
 };
-  
-DOMSelector.buttons.choccoMintThemeButton.addEventListener("click", function(event){
 
-    DOMSelector.body.classList.add("chocco-mint-theme");
-    DOMSelector.body.classList.remove("dark-theme");
-    DOMSelector.body.classList.remove("purple-mint-theme");
-    
-});
-
-DOMSelector.buttons.darkThemeButton.addEventListener("click", function(event){
-
-    DOMSelector.body.classList.remove("chocco-mint-theme");
-    DOMSelector.body.classList.add("dark-theme");
-    DOMSelector.body.classList.remove("purple-mint-theme");
-  
-});
-
-DOMSelector.buttons.purpleMintThemeButton.addEventListener("click", function(event){
+function themeSwitcher(cssThemeClass) {
 
 	DOMSelector.body.classList.remove("chocco-mint-theme");
 	DOMSelector.body.classList.remove("dark-theme");
-	DOMSelector.body.classList.add("purple-mint-theme");
+	DOMSelector.body.classList.remove("purple-mint-theme");
+	DOMSelector.body.classList.add(cssThemeClass);
+
+}
+
+DOMSelector.buttons.choccoMintThemeButton.addEventListener("click", function (event) {
+
+	themeSwitcher("chocco-mint-theme");
+
+});
+
+DOMSelector.buttons.darkThemeButton.addEventListener("click", function (event) {
+
+	themeSwitcher("dark-theme");
+
+});
+
+DOMSelector.buttons.purpleMintThemeButton.addEventListener("click", function (event) {
+
+	themeSwitcher("purple-mint-theme");
 
 });
 
 
-DOMSelector.buttons.ansiButton.addEventListener("click", function(event){
+DOMSelector.buttons.ansiButton.addEventListener("click", function (event) {
 
-  console.log("ansi click");
-  const wanted_thing = keycaps.filter((card) => card.isoOrANSI === "ansi" || "both");
-  cardCreate(wanted_thing);
-  if (wanted_thing.length === 0) {
-    DOMSelector.cardHolder.innerHTML=`
+	console.log("ansi click");
+	const wanted_thing = keycaps.filter((card) => card.isoOrANSI === "ansi" || "both");
+	cardCreate(wanted_thing);
+	if (wanted_thing.length === 0) {
+		DOMSelector.cardHolder.innerHTML = `
     <p>nothing fits that womp womp</p>
     `
-  };
+	};
 
 });
 
-DOMSelector.buttons.isoButton.addEventListener("click", function(event){
+DOMSelector.buttons.isoButton.addEventListener("click", function (event) {
 
-  console.log("iso click");
-  const wanted_thing = keycaps.filter((card) => card.isoOrANSI === "iso" || "both");
-  cardCreate(wanted_thing);
-  if (wanted_thing.length === 0) {
-    DOMSelector.cardHolder.innerHTML=`
+	console.log("iso click");
+	const wanted_thing = keycaps.filter((card) => card.isoOrANSI === "iso" || "both");
+	cardCreate(wanted_thing);
+	if (wanted_thing.length === 0) {
+		DOMSelector.cardHolder.innerHTML = `
     <p>nothing fits that womp womp</p>
     `
-  };
+	};
 
 });
 
-DOMSelector.buttons.oemButton.addEventListener("click", function(event){
+DOMSelector.buttons.oemButton.addEventListener("click", function (event) {
 
-  console.log("oem click");
-  const wanted_thing = keycaps.filter((card) => card.profile === oem);
-  cardCreate(wanted_thing);
-  if (wanted_thing.length === 0) {
-    cardHolder.innerHTML=`
+	console.log("oem click");
+	const wanted_thing = keycaps.filter((card) => card.profile === oem);
+	cardCreate(wanted_thing);
+	if (wanted_thing.length === 0) {
+		cardHolder.innerHTML = `
     <p>nothing fits that womp womp</p>
     `
-  };
+	};
 
 });
 
-DOMSelector.buttons.defaultSortButton.addEventListener("click", function(event){
+DOMSelector.buttons.defaultSortButton.addEventListener("click", function (event) {
 
-  console.log("click")
-  DOMSelector.cardHolder.innerHTML="";
-  cardCreate(keycaps);
-  
+	console.log("click")
+	DOMSelector.cardHolder.innerHTML = "";
+	cardCreate(keycaps);
+
 })
 
 //DOMSelector.buttons.inStockButtonButton.addEventListener("click", function(event){
